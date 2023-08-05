fe14cedb57442940f020a8f10c86d92830a72eeb
eftenow <mass4cre@icloud.com>
2023-08-05 17:04:43 +0300

    added discount field to the product card

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { AiFillShopping } from 'react-icons/ai'
+import { BiShoppingBag } from 'react-icons/bi'
+import './ProductCard.css';
 
 
 const ProductCard = () => {
@@ -7,13 +8,17 @@ const ProductCard = () => {
         <article className='product-card'>
             <img className="product-image" src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/cabc3285-a6b3-40af-9261-8cdf403166ad/gt-jump-basketball-shoes-Fc9gSw.png" alt='Product' />
             <div className='card-details'>
-                <h3 className='card-title'>Nike Shoes</h3>
+                <p className="brand-name">Nike</p>
+                <p className='card-title'>Nike Air Force 1</p>
                 <p className='card-price'>
                     <div className='price'>
-                        <del>$120</del> $ <span>100</span>
+                    <span className='discounted-price'>$100</span> <del>$120</del> 
                     </div>
                     <div className='purchase-item'>
-                        <AiFillShopping className='purchase-item-icon' />
+                        <BiShoppingBag className='purchase-item-icon' />
+                    </div>
+                    <div className="discount">
+                        <span>-10%</span>
                     </div>
                 </p>
             </div>
