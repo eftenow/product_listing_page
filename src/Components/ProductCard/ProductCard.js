5963ac2df46d99d83e65b1cc482e43e50f816023
eftenow <mass4cre@icloud.com>
2023-08-05 15:59:40 +0300

    refactoring products page for cleaner code

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import { AiFillShopping } from 'react-icons/ai'
+
 
 const ProductCard = () => {
-
+    return (
+        <article className='product-card'>
+            <img className="product-image" src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/cabc3285-a6b3-40af-9261-8cdf403166ad/gt-jump-basketball-shoes-Fc9gSw.png" alt='Product' />
+            <div className='card-details'>
+                <h3 className='card-title'>Nike Shoes</h3>
+                <p className='card-price'>
+                    <div className='price'>
+                        <del>$120</del> $ <span>100</span>
+                    </div>
+                    <div className='purchase-item'>
+                        <AiFillShopping className='purchase-item-icon' />
+                    </div>
+                </p>
+            </div>
+        </article>
+    )
 }
 
 
--- src/Containers/Products/Products.js
+++ src/Containers/Products/Products.js
@@ -1,24 +1,14 @@
+import ProductCard from '../../Components/ProductCard/ProductCard';
 import './Products.css';
-import { AiFillShopping } from 'react-icons/ai'
 
 const Products = () => {
     return (
         <>
-            <section className='card-container'>
-                <article className='product-card'>
-                    <img className="product-image" src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/cabc3285-a6b3-40af-9261-8cdf403166ad/gt-jump-basketball-shoes-Fc9gSw.png" alt='Product' />
-                    <div className='card-details'>
-                        <h3 className='card-title'>Nike Shoes</h3>
-                        <p className='card-price'>
-                            <div className='price'>
-                                <del>$120</del> $ <span>100</span>
-                            </div>
-                            <div className='purchase-item'>
-                                <AiFillShopping className='purchase-item-icon' />
-                            </div>
-                        </p>
-                    </div>
-                </article>
+            <section className='cards-container'>
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+        
             </section>
         </>
     )
