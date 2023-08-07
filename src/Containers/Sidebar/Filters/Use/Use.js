72cd05d571274806264214dbd7d97a5bde966f46
eftenow <mass4cre@icloud.com>
2023-08-07 10:55:00 +0300

    new "Use" filter, that fiters by the use of the item - formal, informal, sport, etc

---
+++ src/Containers/Sidebar/Filters/Use/Use.js
@@ -0,0 +1,28 @@
+import Input from "../../../../Components/Input/Input";
+import '../Filters.css';
+
+const Use = ({ filterValues, handleFilterChange }) => {
+    const uses = ['sport', 'formal', 'casual']; 
+  
+    return (
+      <div className='use-filter-container'>
+        <h2 className="sidebar-title">Use</h2>
+  
+        <div className='filter-items-wrapper'>
+          {uses.map((use) => (
+            <Input
+              key={use}
+              value={use}
+              title={use.charAt(0).toUpperCase() + use.slice(1)}
+              name="use"
+              filterValues={filterValues}
+              onChange={handleFilterChange}
+            />
+          ))}
+        </div>
+      </div>
+    )
+  }
+
+
+  export default Use;
